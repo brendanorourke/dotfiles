8061224bd7593eb1caabd3f0e1dfaa3516694d75
Brendan O'Rourke <brendanorourke@users.noreply.github.com>
2018-06-09 17:48:43 -0700

    Add init functionality (#4)

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -57,8 +57,67 @@ function link_do() {
 
 
 ###########################################
-# INSTALL SYSTEM REQUIRMENTS
+# INITIALIZE SYSTEM
 ###########################################
+
+
+DOTFILES_SCRIPT_RUNNING=1
+
+function cleanup {
+  unset DOTFILES_SCRIPT_RUNNING
+}
+
+trap cleanup EXIT
+
+# Initialize.
+init_file=$DOTFILES/caches/init/selected
+
+function init_files() {
+  local i f dirname oses os opt remove
+  dirname="$(dirname "$1")"
+  f=("$@")
+  menu_options=(); menu_selects=()
+  for i in "${!f[@]}"; do menu_options[i]="$(basename "${f[i]}")"; done
+  if [[ -e "$init_file" ]]; then
+    # Read cache file if possible
+    IFS=$'\n' read -d '' -r -a menu_selects < "$init_file"
+  else
+    # Otherwise default to all scripts not specifically for other OSes
+    oses=($(get_os 1))
+    for opt in "${menu_options[@]}"; do
+      remove=
+      for os in "${oses[@]}"; do
+        [[ "$opt" =~ (^|[^a-z])$os($|[^a-z]) ]] && remove=1 && break
+      done
+      [[ "$remove" ]] || menu_selects=("${menu_selects[@]}" "$opt")
+    done
+  fi
+  prompt_menu "Run the following init scripts?" $prompt_delay
+  # Write out cache file for future reading.
+  rm "$init_file" 2>/dev/null
+  for i in "${!menu_selects[@]}"; do
+    echo "${menu_selects[i]}" >> "$init_file"
+    echo "$dirname/${menu_selects[i]}"
+  done
+}
+
+function init_do() {
+  e_header "Sourcing $(basename "$2")"
+  source "$2"
+}
+
+###########################################
+# DO STUFF!
+###########################################
+
+# Set the prompt delay to be longer for the very first run.
+export prompt_delay=5; is_dotfiles_bin || prompt_delay=15
+
+# Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
+# Note that this doesn't work with Homebrew, since brew explicitly invalidates
+# the sudo timestamp, which is probably wise.
+# See https://gist.github.com/cowboy/3118588
+while true; do sudo -n true; sleep 10; kill -0 "$$" || exit; done 2>/dev/null &
 
 #
 # Ensure that we can actually, like, compile anything.
@@ -96,33 +155,6 @@ if [[ ! "$(type -P curl)" ]] && is_ubuntu; then
 fi
 
 #
-# If vim is not installed, install it
-# Ubuntu only, since vim ships with macOS
-#
-if [[ ! "$(type -P vim)" ]] && is_ubuntu; then
-  function git_header() { e_header "Installing vim…"; }
-
-  function git_install() {
-    sudo apt-get -qq install vim
-  }
-
-  install_stuff "vim"
-fi
-
-#
-# Install vim bundles
-#
-function vimbundles_header() { e_header "Installing vim bundles…"; }
-
-function vimbundles_install() {
-  mkdir -p ~/.vim/bundle
-  if [[ ! -e ~/.vim/bundle/vundle ]]; then
-    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-  fi
-  vim +BundleInstall! +BundleClean +qall 2&> /dev/null
-}
-
-#
 # Install oh-my-zsh
 #
 function ohmyzsh_header() { e_header "Installing Oh My Zsh…"; }
@@ -132,12 +164,11 @@ function ohmyzsh_install() {
   cp $DOTFILES/external/themes/bullet-train.zsh-theme ~/.oh-my-zsh/themes/bullet-train.zsh-theme
 }
 
+install_stuff "ohmyzsh"
 
-###########################################
-# INITIALIZE DOTFILES
-###########################################
-
-
+#
+# Initialize DOTFILES
+#
 if [[ ! -d $DOTFILES ]]; then
   e_header "Downloading dotfiles…"
   git clone --recursive git://github.com/$GITHUB_USER/dotfiles.git $DOTFILES
@@ -174,11 +205,12 @@ backup=""
 # Execute code for each file in these subdirectories.
 do_stuff "copy"
 do_stuff "link"
-
-install_stuff "ohmyzsh"
-install_stuff "vimbundles"
+do_stuff "init"
 
 # Alert if backups were made.
 if [[ "$backup" ]]; then
   echo -e "\nBackups were moved to ~/${backup_dir#$HOME/}"
 fi
+
+# Complete
+e_header "dotfiles execution complete!"
--- bin/utils.sh
+++ bin/utils.sh
@@ -72,6 +72,20 @@ function assert() {
 }
 
 #
+# Test if the dotfiles script is currently
+#
+function is_dotfiles_running() {
+  [[ "$DOTFILES_SCRIPT_RUNNING" ]] || return 1
+}
+
+#
+# Test if this script was run via the "dotfiles" bin script (vs. via curl/wget)
+#
+function is_dotfiles_bin() {
+  [[ "$(basename $0 2>/dev/null)" == dotfiles ]] || return 1
+}
+
+#
 # OS detection
 #
 function is_osx() {
@@ -82,10 +96,178 @@ function is_ubuntu() {
   [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || return 1
 }
 
+function is_ubuntu_desktop() {
+  dpkg -l ubuntu-desktop >/dev/null 2>&1 || return 1
+}
+
 function get_os() {
-  for os in osx ubuntu; do
+  for os in osx ubuntu ubuntu_desktop; do
     is_$os; [[ $? == "${1:-0}" ]] && echo $os
   done
+}
+
+#
+# Display a fancy multi-select menu.
+# Inspired by http://serverfault.com/a/298312
+#
+function prompt_menu() {
+  local exitcode prompt choices nums i n
+  exitcode=0
+  if [[ "$2" ]]; then
+    _prompt_menu_draws "$1"
+    read -t $2 -n 1 -sp "Press ENTER or wait $2 seconds to continue, or press any other key to edit."
+    exitcode=$?
+    echo ""
+  fi 1>&2
+  if [[ "$exitcode" == 0 && "$REPLY" ]]; then
+    prompt="Toggle options (Separate options with spaces, ENTER when done): "
+    while _prompt_menu_draws "$1" 1 && read -rp "$prompt" nums && [[ "$nums" ]]; do
+      _prompt_menu_adds $nums
+    done
+  fi 1>&2
+  _prompt_menu_adds
+}
+
+function _prompt_menu_iter() {
+  local i sel state
+  local fn=$1; shift
+  for i in "${!menu_options[@]}"; do
+    state=0
+    for sel in "${menu_selects[@]}"; do
+      [[ "$sel" == "${menu_options[i]}" ]] && state=1 && break
+    done
+    $fn $state $i "$@"
+  done
+}
+
+function _prompt_menu_draws() {
+  e_header "$1"
+  _prompt_menu_iter _prompt_menu_draw "$2"
+}
+
+function _prompt_menu_draw() {
+  local modes=(error success)
+  if [[ "$3" ]]; then
+    e_${modes[$1]} "$(printf "%2d) %s\n" $(($2+1)) "${menu_options[$2]}")"
+  else
+    e_${modes[$1]} "${menu_options[$2]}"
+  fi
+}
+
+function _prompt_menu_adds() {
+  _prompt_menu_result=()
+  _prompt_menu_iter _prompt_menu_add "$@"
+  menu_selects=("${_prompt_menu_result[@]}")
+}
+
+function _prompt_menu_add() {
+  local state i n keep match
+  state=$1; shift
+  i=$1; shift
+  for n in "$@"; do
+    if [[ $n =~ ^[0-9]+$ ]] && (( n-1 == i )); then
+      match=1; [[ "$state" == 0 ]] && keep=1
+    fi
+  done
+  [[ ! "$match" && "$state" == 1 || "$keep" ]] || return
+  _prompt_menu_result=("${_prompt_menu_result[@]}" "${menu_options[i]}")
+}
+
+#
+# Array mapper. Calls map_fn for each item ($1) and index ($2) in array, and
+# prints whatever map_fn prints. If map_fn is omitted, all input array items
+# are printed.
+# Usage: array_map array_name [map_fn]
+#
+function array_map() {
+  local __i__ __val__ __arr__=$1; shift
+  for __i__ in $(eval echo "\${!$__arr__[@]}"); do
+    __val__="$(eval echo "\"\${$__arr__[__i__]}\"")"
+    if [[ "$1" ]]; then
+      "$@" "$__val__" $__i__
+    else
+      echo "$__val__"
+    fi
+  done
+}
+
+#
+# Print bash array in the format "i <val>" (one per line) for debugging.
+#
+function array_print() { array_map $1 __array_print; }
+function __array_print() { echo "$2 <$1>"; }
+
+#
+# Array filter. Calls filter_fn for each item ($1) and index ($2) in array_name
+# array, and prints all values for which filter_fn returns a non-zero exit code
+# to stdout. If filter_fn is omitted, input array items that are empty strings
+# will be removed.
+# Usage: array_filter array_name [filter_fn]
+# Eg. mapfile filtered_arr < <(array_filter source_arr)
+#
+function array_filter() { __array_filter 1 "$@"; }
+
+# Works like array_filter, but outputs array indices instead of array items.
+function array_filter_i() { __array_filter 0 "$@"; }
+
+# The core function. Wheeeee.
+function __array_filter() {
+  local __i__ __val__ __mode__ __arr__
+  __mode__=$1; shift; __arr__=$1; shift
+  for __i__ in $(eval echo "\${!$__arr__[@]}"); do
+    __val__="$(eval echo "\${$__arr__[__i__]}")"
+    if [[ "$1" ]]; then
+      "$@" "$__val__" $__i__ >/dev/null
+    else
+      [[ "$__val__" ]]
+    fi
+    if [[ "$?" == 0 ]]; then
+      if [[ $__mode__ == 1 ]]; then
+        eval echo "\"\${$__arr__[__i__]}\""
+      else
+        echo $__i__
+      fi
+    fi
+  done
+}
+
+#
+# Array join. Joins array ($1) items on string ($2).
+#
+function array_join() { __array_join 1 "$@"; }
+
+# Works like array_join, but removes empty items first.
+function array_join_filter() { __array_join 0 "$@"; }
+
+# Core function for the above
+function __array_join() {
+  local __i__ __val__ __out__ __init__ __mode__ __arr__
+  __mode__=$1; shift; __arr__=$1; shift
+  for __i__ in $(eval echo "\${!$__arr__[@]}"); do
+    __val__="$(eval echo "\"\${$__arr__[__i__]}\"")"
+    if [[ $__mode__ == 1 || "$__val__" ]]; then
+      [[ "$__init__" ]] && __out__="$__out__$@"
+      __out__="$__out__$__val__"
+      __init__=1
+    fi
+  done
+  [[ "$__out__" ]] && echo "$__out__"
+}
+
+#
+# Do something n times.
+#
+function n_times() {
+  local max=$1; shift
+  local i=0; while [[ $i -lt $max ]]; do "$@"; i=$((i+1)); done
+}
+
+#
+# Do something n times, passing along the array index.
+#
+function n_times_i() {
+  local max=$1; shift
+  local i=0; while [[ $i -lt $max ]]; do "$@" "$i"; i=$((i+1)); done
 }
 
 #
