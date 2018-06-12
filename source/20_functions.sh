a31bf9abebb16de32a97b668d1ac2644c8492939
Brendan ORourke <orourkebm@gmail.com>
2018-06-12 10:00:21 -0700

    Misc formatting

--- source/20_functions.sh
+++ source/20_functions.sh
@@ -1,20 +1,15 @@
-#
 # Simple success/fail prettifiers
-#
 function fail()    { printf "\r\033[2K [\033[0;31mFAIL\033[0m] $1\n]]]" && exit 1;}
 function success() { printf "\r\033[2K [\033[00;32mOK\033[0m ] $1\n]]]"; }
 
-#
+
 # Check if command is installed
-#
 function check_install() {
   if ! type "$1" > /dev/null; then
     fail "Please install $1..."
   fi
 }
 
-#
 # Various convenience functions for navigation
-#
 function makeDirAndChange() { mkdir $1 && cd $1; }
 function lsAndGrep()        { ls -lAh | grep $1; }
