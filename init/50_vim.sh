91f071dab3d3b4e006fd43db4043cf3b725cc980
Brendan ORourke <orourkebm@gmail.com>
2018-06-17 02:45:20 -0700

    Update vim script

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -1,11 +1,33 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+  && . "../utils"
+
+declare -r BUNDLE_DIR="$HOME/.vim/bundle"
+declare -r VUNDLE_DIR="$HOME/.vim/bundle/vundle"
+declare -r VUNDLE_GIT="http://github.com/gmarik/vundle.git "
+
 # Backups, swaps and undos are stored here.
-mkdir -p $DOTFILES/caches/vim
+execute \
+  "mkdir -p $DOTFILES/caches/vim" \
+  "create /caches/vim"
 
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
-  mkdir -p ~/.vim/bundle
-  if [[ ! -e ~/.vim/bundle/vundle ]]; then
-    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-  fi
-  vim +BundleInstall! +BundleClean +qall 2&> /dev/null
+  
+  mkdir -p $BUNDLE_DIR
+
+  execute \
+    "rm -rf '$VUNDLE_DIR' \
+      && git clone --quiet '$VUNDLE_GIT' '$VUNDLE_DIR'" \
+    "download vundle"
+  
+  execute \
+    "vim +BundleInstall! +BundleClean +qall 2&> /dev/null" \
+    'install bundles'
+
+else
+
+  print_error "vim not installed"
+
 fi
