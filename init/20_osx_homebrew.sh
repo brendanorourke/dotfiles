1c74bc441d3351fbd1941be389ffa88670787257
Brendan O'Rourke <brendanorourke@users.noreply.github.com>
2018-06-17 02:54:04 -0700

    Brendan/utils refactor (#9)
    * Cleanup superfluous util functions
    
    * Update /bin/(dotfiles|utils)
    
    * Fix prompt
    
    * Fix homebrew scripts
    
    * Fix link text
    
    * Dont wrap init files in execute, source directly
    
    * Send homebrew output to /dev/null
    
    * Update deprecated git flag
    
    * Update headers
    
    * Update vim script
    
    * Split oh-my-zsh and theme into separate executions

--- init/20_osx_homebrew.sh
+++ init/20_osx_homebrew.sh
@@ -1,56 +1,24 @@
 # OSX-only stuff. Abort if not OSX.
 is_osx || return 1
 
-# Install Homebrew.
+print_header "Running homebrew health checks"
+
+# Install Homebrew if not already installed
 if [[ ! "$(type -P brew)" ]]; then
-  e_header "Installing Homebrew"
-  true | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  
+  execute \
+    "true | ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\"" \
+    "install homebrew"
+
 fi
 
 # Exit if, for some reason, Homebrew is not installed.
-[[ ! "$(type -P brew)" ]] && e_error "Homebrew failed to install." && return 1
+[[ ! "$(type -P brew)" ]] && print_error "homebrew not installed" && return 1
 
-e_header "Updating Homebrew"
-brew doctor
-brew update
+execute \
+  "brew doctor &>/dev/null" \
+  "brew doctor"
 
-############################################
-# Functions used in subsequent init scripts.
-############################################
-
-# Tap Homebrew kegs.
-function brew_tap_kegs() {
-  kegs=($(setdiff "${kegs[*]}" "$(brew tap)"))
-  if (( ${#kegs[@]} > 0 )); then
-    e_header "Tapping Homebrew kegs: ${kegs[*]}"
-    for keg in "${kegs[@]}"; do
-      brew tap $keg
-      e_success "$keg tapped"
-    done
-  fi
-}
-
-# Install Homebrew casks.
-function brew_install_casks() {
-  casks=($(setdiff "${casks[*]}" "$(brew cask list 2>/dev/null)"))
-  if (( ${#casks[@]} > 0 )); then
-    e_header "Installing Homebrew casks: ${casks[*]}"
-    for cask in "${casks[@]}"; do
-      brew cask install $cask
-      e_success "$cask installed"
-    done
-    brew cask cleanup
-  fi
-}
-
-# Install Homebrew recipes.
-function brew_install_recipes() {
-  recipes=($(setdiff "${recipes[*]}" "$(brew list)"))
-  if (( ${#recipes[@]} > 0 )); then
-    e_header "Installing Homebrew recipes: ${recipes[*]}"
-    for recipe in "${recipes[@]}"; do
-      brew install $recipe
-      e_success "$recipe installed"
-    done
-  fi
-}
+execute \
+  "brew update &>/dev/null" \
+  "brew update"
--- init/30_osx_homebrew_casks.sh
+++ init/30_osx_homebrew_casks.sh
@@ -1,8 +1,57 @@
 # Abort if not OSX
 is_osx || return 1
 
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../bin/utils"
+
 # Exit if Homebrew is not installed.
-[[ ! "$(type -P brew)" ]] && e_error "Brew casks need Homebrew to install." && return 1
+[[ ! "$(type -P brew)" ]] && print_error "brew casks require homebrew to install." && return 1
+
+# Tap Homebrew kegs.
+function brew_tap_kegs() {
+  
+  print_header "Tapping Homebrew Kegs"
+
+  kegs=($(setdiff "${kegs[*]}" "$(brew tap)"))
+  
+  if (( ${#kegs[@]} > 0 )); then
+  
+    for keg in "${kegs[@]}"; do
+  
+      execute \
+        "brew tap $keg &>/dev/null" \
+        "$keg"
+  
+    done
+  
+  fi
+}
+
+# Install Homebrew casks.
+function brew_install_casks() {
+  
+  casks=($(setdiff "${casks[*]}" "$(brew cask list 2>/dev/null)"))
+  
+  if (( ${#casks[@]} > 0 )); then
+  
+    #e_header "Installing Homebrew casks: ${casks[*]}"
+    print_header "Installing Homebrew Casks"
+
+    for cask in "${casks[@]}"; do
+  
+      execute \
+        "brew cask install $cask &>/dev/null" \
+        "$cask"
+
+    done
+    
+    execute \
+        "brew cask cleanup &>/dev/null" \
+        "cleanup"
+  
+  fi
+
+}
 
 kegs=(
     caskroom/cask
