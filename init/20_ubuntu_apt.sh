e551b42bbe7c7a604e49f0db8cf4008dd9bb2db5
Brendan O'Rourke <orourkebm@gmail.com>
2018-06-18 01:01:00 -0700

    Misc formatting and comments

--- init/20_ubuntu_apt.sh
+++ init/20_ubuntu_apt.sh
@@ -1,27 +1,25 @@
 # Ubuntu-only stuff. Abort if not Ubuntu.
 is_ubuntu || return 1
 
+# ---------------------------
+# VARS AND CONSTS
+# ---------------------------
+
 apt_keys=()
+apt_packages=()
 apt_source_files=()
 apt_source_texts=()
-apt_packages=()
 deb_installed=()
 deb_sources=()
 
-installers_path="$DOTFILES/caches/installers"
+INSTALLERS_PATH="$DOTFILES/caches/installers"
 
 # Ubuntu distro release name, eg. "xenial"
-release_name=$(lsb_release -c | awk "{print $2}")
+RELEASE_NAME=$(lsb_release -c | awk "{print $2}")
 
-function add_ppa() {
-  apt_source_texts+=($1)
-  IFS=':/' eval 'local parts=($1)'
-  apt_source_files+=("${parts[1]}-ubuntu-${parts[2]}-$release_name")
-}
-
-#############################
-# WHAT DO WE NEED TO INSTALL?
-#############################
+# ---------------------------
+# ADD PACKAGES TO INSTALL
+# ---------------------------
 
 # Misc.
 apt_packages+=()
@@ -31,38 +29,98 @@ is_ubuntu_desktop && apt_packages+=(vim-gnome)
 
 
 if is_ubuntu_desktop; then
-    # Misc
+
   apt_packages+=()
   apt_packages+=()
+
 fi
 
-function other_stuff() {
-  # Install misc bins from zip file.
-  install_from_zip ngrok "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip"
-  install_from_zip exa-linux-x86_64 "https://github.com/ogham/exa/releases/download/v0.8.0/exa-linux-x86_64-0.8.0.zip"
+# ---------------------------
+# HELPER FUNCTIONS
+# ---------------------------
+
+function add_ppa() {
+  
+  apt_source_texts+=($1)
+  
+  IFS=':/' eval 'local parts=($1)'
+  
+  apt_source_files+=("${parts[1]}-ubuntu-${parts[2]}-$RELEASE_NAME")
+
 }
 
-####################
+# install bins from zip file
+function install_from_zip() {
+  local name=$1 url=$2 bins b zip tmp
+  
+  shift 2; bins=("$@"); [[ "${#bins[@]}" == 0 ]] && bins=($name)
+  
+  if [[ ! "$(which $name)" ]]; then
+  
+    mkdir -p "$INSTALLERS_PATH"
+    e_header "Installing $name"
+    zip="$INSTALLERS_PATH/$(echo "$url" | sed 's#.*/##')"
+    wget -O "$zip" "$url"
+    tmp=$(mktemp -d)
+    unzip "$zip" -d "$tmp"
+  
+    for b in "${bins[@]}"; do
+  
+      sudo cp "$tmp/$b" "/usr/local/bin/$(basename $b)"
+  
+    done
+  
+    rm -rf $tmp
+    e_success "installed $name"
+  
+  else
+  
+    e_arrow "skipping $name, already installed"
+  
+  fi
+  
+}
+
+# Install misc bins from zip file.
+function other_stuff() {
+
+  install_from_zip ngrok "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip"
+  install_from_zip exa-linux-x86_64 "https://github.com/ogham/exa/releases/download/v0.8.0/exa-linux-x86_64-0.8.0.zip"
+
+}
+
+# ---------------------------
 # ACTUALLY DO THINGS
-####################
+# ---------------------------
 
 # Add APT keys.
 keys_cache=$DOTFILES/caches/init/apt_keys
 IFS=$'\n' GLOBIGNORE='*' command eval 'setdiff_cur=($(<$keys_cache))'
+
 setdiff_new=("${apt_keys[@]}"); setdiff; apt_keys=("${setdiff_out[@]}")
 unset setdiff_new setdiff_cur setdiff_out
 
 if (( ${#apt_keys[@]} > 0 )); then
+
   e_header "Adding APT keys (${#apt_keys[@]})"
+
   for key in "${apt_keys[@]}"; do
+
     e_arrow "$key"
+
     if [[ "$key" =~ -- ]]; then
+
       sudo apt-key adv $key
+
     else
+
       wget -qO- $key | sudo apt-key add -
+
     fi && \
     echo "$key" >> $keys_cache
+
   done
+
 fi
 
 # Add APT sources.
@@ -70,18 +128,29 @@ function __temp() { [[ ! -e /etc/apt/sources.list.d/$1.list ]]; }
 source_i=($(array_filter_i apt_source_files __temp))
 
 if (( ${#source_i[@]} > 0 )); then
+ 
   e_header "Adding APT sources (${#source_i[@]})"
+  
   for i in "${source_i[@]}"; do
+  
     source_file=${apt_source_files[i]}
     source_text=${apt_source_texts[i]}
+  
     if [[ "$source_text" =~ ppa: ]]; then
+  
       e_arrow "$source_text"
       sudo add-apt-repository -y $source_text
+  
     else
+  
       e_arrow "$source_file"
       sudo sh -c "echo '$source_text' > /etc/apt/sources.list.d/$source_file.list"
+  
     fi
+  
   done
+
+
 fi
 
 # Update APT.
@@ -90,10 +159,15 @@ sudo apt-get -qq update
 
 # Only do a dist-upgrade on initial install, otherwise do an upgrade.
 e_header "Upgrading APT"
+
 if is_dotfiles_bin; then
+
   sudo apt-get -qqy upgrade
+
 else
+
   sudo apt-get -qqy dist-upgrade
+
 fi
 
 # Install APT packages.
@@ -101,13 +175,19 @@ installed_apt_packages="$(dpkg --get-selections | grep -v deinstall | awk 'BEGIN
 apt_packages=($(setdiff "${apt_packages[*]}" "$installed_apt_packages"))
 
 if (( ${#apt_packages[@]} > 0 )); then
+
   e_header "Installing APT packages (${#apt_packages[@]})"
+
   for package in "${apt_packages[@]}"; do
+
     e_arrow "$package"
     [[ "$(type -t preinstall_$package)" == function ]] && preinstall_$package
+
     sudo apt-get -qq install "$package" && \
     [[ "$(type -t postinstall_$package)" == function ]] && postinstall_$package
+
   done
+
 fi
 
 # Install debs via dpkg
@@ -115,39 +195,23 @@ function __temp() { [[ ! -e "$1" ]]; }
 deb_installed_i=($(array_filter_i deb_installed __temp))
 
 if (( ${#deb_installed_i[@]} > 0 )); then
-  mkdir -p "$installers_path"
+
+  mkdir -p "$INSTALLERS_PATH"
+
   e_header "Installing debs (${#deb_installed_i[@]})"
+
   for i in "${deb_installed_i[@]}"; do
+
     e_arrow "${deb_installed[i]}"
     deb="${deb_sources[i]}"
     [[ "$(type -t "$deb")" == function ]] && deb="$($deb)"
-    installer_file="$installers_path/$(echo "$deb" | sed 's#.*/##')"
+    installer_file="$INSTALLERS_PATH/$(echo "$deb" | sed 's#.*/##')"
     wget -O "$installer_file" "$deb"
     sudo dpkg -i "$installer_file"
-  done
-fi
 
-# install bins from zip file
-function install_from_zip() {
-  local name=$1 url=$2 bins b zip tmp
-  shift 2; bins=("$@"); [[ "${#bins[@]}" == 0 ]] && bins=($name)
-  if [[ ! "$(which $name)" ]]; then
-    mkdir -p "$installers_path"
-    e_header "Installing $name"
-    zip="$installers_path/$(echo "$url" | sed 's#.*/##')"
-    wget -O "$zip" "$url"
-    tmp=$(mktemp -d)
-    unzip "$zip" -d "$tmp"
-    for b in "${bins[@]}"; do
-      sudo cp "$tmp/$b" "/usr/local/bin/$(basename $b)"
-    done
-    rm -rf $tmp
-    e_success "installed $name"
-  else
-    e_arrow "skipping $name, already installed"
-  fi
-  
-}
+  done
+
+fi
 
 # Run anything else that may need to be run.
 e_header "Installing misc packages…"
