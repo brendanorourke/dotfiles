aa16ae27879bcb80e177c257ebc11f8b63bd6482
Brendan O'Rourke <brendanorourke@users.noreply.github.com>
2018-07-01 12:04:42 -0700

    Update 50_sift.sh; fix variable exports (#11)

--- source/50_sift.sh
+++ source/50_sift.sh
@@ -4,21 +4,32 @@ if [ -f ~/.sift ]; then
     source ~/.sift
 fi
 
-##############
-# Path Updates
-##############
 
+# ----------------
+# Export Variables
+# ----------------
+export SE_MACHINE=$SE_MACHINE
+export SE_MACHINE_USERNAME=$SE_MACHINE_USERNAME
+export SIFT_ACCOUNT_ID=$SIFT_ACCOUNT_ID
+export SIFT_REST_KEY=$SIFT_REST_KEY
+
+
+# ----------------
+# Path Updates
+# ----------------
 export PYTHONPATH="${PYTHONPATH}:$HOME/dev/SE_tools"
 
-##############
+
+# ----------------
 # Aliases
-##############
+# ----------------
 
 alias se="ssh $SE_MACHINE_USERNAME@$SE_MACHINE"
 
-##############
+
+# ----------------
 # Functions
-##############
+# ----------------
 
 # Copy files over SSH
 function copy_to_se()   { scp $1 $SE_MACHINE_USERNAME@$SE_MACHINE:/home/$SE_MACHINE_USERNAME/$2; }
