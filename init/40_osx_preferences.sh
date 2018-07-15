965e27873d9e60f74206008f2f7515249036401f
Brendan O'Rourke <orourkebm@gmail.com>
2018-07-15 13:28:02 -0700

    Create osx_preferences.sh-- add Finder prefs

---
+++ init/40_osx_preferences.sh
@@ -0,0 +1,41 @@
+# Abort if not OSX
+is_osx || return 1
+
+# ---------------------------
+# FINDER
+# ---------------------------
+
+print_header "Set Preferences: Finder"
+
+execute "defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true && \
+         defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true && \
+         defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true" \
+    "Automatically open a new Finder window when a volume is mounted"
+
+execute "defaults write com.apple.finder _FXShowPosixPathInTitle -bool true" \
+    "Use full POSIX path as window title"
+
+execute "defaults write com.apple.finder DisableAllAnimations -bool true" \
+    "Disable all animations"
+
+execute "defaults write com.apple.finder WarnOnEmptyTrash -bool false" \
+    "Disable the warning before emptying the Trash"
+
+execute "defaults write com.apple.finder FXDefaultSearchScope -string 'SCcf'" \
+    "Search the current directory by default"
+
+execute "defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false" \
+    "Disable warning when changing a file extension"
+
+execute "defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true && \
+         defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true && \
+         defaults write com.apple.finder ShowMountedServersOnDesktop -bool true && \
+         defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true" \
+    "Show icons for hard drives, servers, and removable media on the desktop"
+
+execute "defaults write -g AppleShowAllExtensions -bool true" \
+    "Show all filename extensions"
+
+
+
+killall "Finder" &> /dev/null
