fbd8fffe45e170775abbf37544939000e4a6167d
Brendan O'Rourke <brendanorourke@users.noreply.github.com>
2018-07-21 10:56:17 -0700

    Brendan/ubuntu init refactor (#16)
    * Split debs and zips from apts into separate init files.
    
    * Various formatting. If you see something, do something.

--- init/30_osx_homebrew_casks.sh
+++ init/30_osx_homebrew_casks.sh
@@ -4,36 +4,74 @@ is_osx || return 1
 # Exit if Homebrew is not installed.
 [[ ! "$(type -P brew)" ]] && print_error "brew casks require homebrew to install." && return 1
 
-# Tap Homebrew kegs.
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# VARS AND CONSTS
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+kegs=(
+    caskroom/cask
+    caskroom/fonts
+)
+
+casks=(
+    docker
+    firefox
+    font-menlo-for-powerline
+    font-meslo-for-powerline
+    iterm2
+    karabiner-elements
+    skype
+    slack
+    spotify
+)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# TAP HOMEBREW KEGS
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
 function brew_tap_kegs() {
-  
-  print_header "Tapping Homebrew Kegs"
 
   kegs=($(setdiff "${kegs[*]}" "$(brew tap)"))
-  
+
+  print_header "Tapping Homebrew Keys (${#kegs[@]})"
+
   if (( ${#kegs[@]} > 0 )); then
-  
+    
     for keg in "${kegs[@]}"; do
-  
+
       execute \
         "brew tap $keg &>/dev/null" \
         "$keg"
-  
+
     done
-  
+    
   fi
+
 }
 
-# Install Homebrew casks.
+brew_tap_kegs
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# INSTALL HOMEBREW CASKS
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+# Show password prompt up-front
+brew cask info this-is-somewhat-annoying 2>/dev/null
+
 function brew_install_casks() {
   
   casks=($(setdiff "${casks[*]}" "$(brew cask list 2>/dev/null)"))
   
+  print_header "Installing Homebrew Casks (${#casks[@]})"
+
   if (( ${#casks[@]} > 0 )); then
   
-    #e_header "Installing Homebrew casks: ${casks[*]}"
-    print_header "Installing Homebrew Casks"
-
     for cask in "${casks[@]}"; do
   
       execute \
@@ -49,27 +87,5 @@ function brew_install_casks() {
   fi
 
 }
-
-kegs=(
-    caskroom/cask
-    caskroom/fonts
-)
-
-brew_tap_kegs
-
-# Show password prompt up-front
-brew cask info this-is-somewhat-annoying 2>/dev/null
-
-casks=(
-    docker
-    firefox
-    font-menlo-for-powerline
-    font-meslo-for-powerline
-    iterm2
-    karabiner-elements
-    skype
-    slack
-    spotify
-)
 
 brew_install_casks
--- init/30_osx_homebrew_recipes.sh
+++ init/30_osx_homebrew_recipes.sh
@@ -4,29 +4,12 @@ is_osx || return 1
 # Exit if homebrew is not installed
 [[ ! "$(type -P brew)" ]] && e_error "Brew recipes need Homebrew to install." && return 1
 
-# Install Homebrew recipes.
-function brew_install_recipes() {
 
-  recipes=($(setdiff "${recipes[*]}" "$(brew list)"))
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# VARS AND CONSTS
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  if (( ${#recipes[@]} > 0 )); then
-    
-    #e_header "Installing Homebrew recipes: ${recipes[*]}"
-    print_header "Installing Homebrew Recipes"
 
-    for recipe in "${recipes[@]}"; do
-      
-      execute \
-        "brew install $recipe" \
-        "$recipe"
-    
-    done
-  
-  fi
-
-}
-
-# Homebrew recipes
 recipes=(
     awscli
     coreutils
@@ -41,5 +24,31 @@ recipes=(
     tree
     wget
 )
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# INSTALL HOMEBREW RECIPES
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+function brew_install_recipes() {
+
+  recipes=($(setdiff "${recipes[*]}" "$(brew list)"))
+
+  print_header "Installing Homebrew Recipes (${#recipes[@]})"
+
+  if (( ${#recipes[@]} > 0 )); then
+
+    for recipe in "${recipes[@]}"; do
+      
+      execute \
+        "brew install $recipe" \
+        "$recipe"
+    
+    done
+  
+  fi
+
+}
 
 brew_install_recipes
