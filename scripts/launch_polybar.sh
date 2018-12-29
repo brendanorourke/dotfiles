a79c39b4325f2c6669f9b1463ef4e322dcd97feb
Brendan O'Rourke <brendanorourke@users.noreply.github.com>
2018-12-29 10:11:59 -0800

    Add script to launch polybar (#36)

---
+++ scripts/launch_polybar.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Terminate already running polybar instances
+killall -q polybar
+
+# Wait until processes are shutdown
+while pgrep -u $UID polybar >/dev/null; do sleep 1; done
+
+# Launch top polybar
+polybar top &
+
+echo "Polybar launched..."
