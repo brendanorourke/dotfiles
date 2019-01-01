16df443968efcefb5f573047d9c5c885d4f9df9e
Brendan O'Rourke <orourkebm@gmail.com>
2019-01-01 12:32:27 -0800

    Add directory checks to polybar install

--- scripts/install_polybar.sh
+++ scripts/install_polybar.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+mkdir -p $DOTFILES/caches/init
 git clone --recursive https://github.com/jaagr/polybar $DOTFILES/caches/init/polybar
 cd $DOTFLIES/caches/init/polybar
 
+rm -rf build
 mkdir build && cd build
+
 cmake ..
 sudo make install
