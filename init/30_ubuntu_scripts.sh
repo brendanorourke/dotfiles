39b783b13ae42577cf05b348db8afa095ee4e345
Brendan O'Rourke <brendanorourke@users.noreply.github.com>
2019-01-21 16:08:08 -0800

    Brendan/adobe font (#59)
    * Add script to install Adobe Source Code Pro font.
    
    * Rename scripts to adhere to naming schema
    
    * Call appropriate scripts during init.

--- init/30_ubuntu_scripts.sh
+++ init/30_ubuntu_scripts.sh
@@ -1,8 +1,15 @@
 # Only run for Ubuntu
 is_ubuntu || return 1
 
+$SCRIPT_DIR="$DOTFILES/scripts"
+
 print_header "Running scripts"
 
 execute \
-  "sudo sh $DOTFILES/scripts/install_polybar.sh" \
-  "polybar"
+  "sudo sh $SCRIPT_DIR/polybar_install.sh" \
+  "install polybar"
+
+execute \
+  "sudo sh $SCRIPT_DIR/adobe_font_install.sh" \
+  "install adobe source code pro"
+
