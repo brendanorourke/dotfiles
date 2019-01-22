d79d0e6898426f225d2a221aacf47c9e375fc9e1
Brendan ORourke <orourkebm@gmail.com>
2019-01-21 16:05:21 -0800

    Rename scripts to adhere to naming schema

--- init/30_ubuntu_scripts.sh
+++ init/30_ubuntu_scripts.sh
@@ -4,5 +4,5 @@ is_ubuntu || return 1
 print_header "Running scripts"
 
 execute \
-  "sudo sh $DOTFILES/scripts/install_polybar.sh" \
+  "sudo sh $DOTFILES/scripts/polybar_install.sh" \
   "polybar"
