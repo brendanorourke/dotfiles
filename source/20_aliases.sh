75f735714d55d48af79c3a6367a9f064c3cd6fac
brendanorourke <orourkebm@gmail.com>
2019-02-01 19:53:54 -0800

    Fix target flag for tma alias

--- source/20_aliases.sh
+++ source/20_aliases.sh
@@ -13,7 +13,7 @@ alias npmclean="rm -rf ./node_modules && npm cache clean && npm install"
 #
 # Tmux helpers
 #
-alias tma="tmux a -s BRENDANOROURKE"
+alias tma="tmux a -t BRENDANOROURKE"
 alias tmn="tmux new -s BRENDANOROURKE -n HipsterSh\#\%"
 alias tmk="tmux kill-session"
 
